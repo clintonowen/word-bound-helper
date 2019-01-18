a4112f3c79be890f10ca57406b0920a2f314597f
Clinton Owen <clint@clintonowen.com>
2019-01-18 17:12:35 -0600

    Move helper functions to utils.js

--- src/actions/words.js
+++ src/actions/words.js
@@ -1,3 +1,12 @@
+import {
+  loadDictionary,
+  hasOnlyPossLetters,
+  includesLetters,
+  hasLettersInPosition,
+  hasNoWrongLetters,
+  rankWords
+} from './utils';
+
 export const FETCH_WORDS_REQUEST = 'FETCH_WORDS_REQUEST';
 export const fetchWordsRequest = () => ({
   type: FETCH_WORDS_REQUEST
@@ -25,33 +34,6 @@ export const fetchWords = data => dispatch => {
     incPosition
   } = data;
   let results = [];
-  let path;
-
-  switch (wordLength) {
-    case '2':
-      path = '/data/dictionaries/2-letter.txt';
-      break;
-    case '3':
-      path = '/data/dictionaries/3-letter.txt';
-      break;
-    case '4':
-      path = '/data/dictionaries/4-letter.txt';
-      break;
-    case '5':
-      path = '/data/dictionaries/5-letter.txt';
-      break;
-    case '6':
-      path = '/data/dictionaries/6-letter.txt';
-      break;
-    case '7':
-      path = '/data/dictionaries/7-letter.txt';
-      break;
-    case '8':
-      path = '/data/dictionaries/8-letter.txt';
-      break;
-    default:
-      break;
-  }
 
   // Validate inputs
   if (!wordLength) {
@@ -76,7 +58,7 @@ export const fetchWords = data => dispatch => {
     incPosition = incPosition.toLowerCase().replace(/\s/g, '').split(',');
   }
 
-  loadDictionary(path)
+  loadDictionary(wordLength)
     .then(dictionary => {
       dictionary.forEach(word => {
         if (hasOnlyPossLetters(word, possLetters) &&
@@ -98,94 +80,3 @@ export const fetchWords = data => dispatch => {
       dispatch(fetchWordsError(err));
     });
 };
-
-function loadDictionary (path) {
-  return new Promise((resolve, reject) => {
-    window.fetch(`${process.env.PUBLIC_URL}${path}`)
-      .then(response => response.text())
-      .then((data) => {
-        let dictionary = [];
-        data.toString().split('\n').forEach(word => {
-          dictionary.push(word);
-        });
-        resolve(dictionary);
-      })
-      .catch(err => reject(err));
-  });
-}
-
-function hasOnlyPossLetters (word, letters) {
-  for (let i = 0; i < word.length; i++) {
-    if (!letters.includes(word[i])) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function includesLetters (word, letters) {
-  if (!letters) {
-    return true;
-  }
-  for (let i = 0; i < letters.length; i++) {
-    let index = word.indexOf(letters[i]);
-    if (index === -1) {
-      return false;
-    } else {
-      word = word.slice(0, index) + word.slice(index + 1);
-    }
-  }
-  return true;
-}
-
-function hasLettersInPosition (word, letters) {
-  if (!letters) {
-    return true;
-  }
-  for (let i = 0; i < letters.length; i++) {
-    if (letters[i] !== '*' && word[i] !== letters[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function hasNoWrongLetters (word, letters) {
-  if (!letters) {
-    return true;
-  }
-  let result = true;
-  letters.forEach(letterSet => {
-    for (let i = 0; i < letterSet.length; i++) {
-      if (letterSet[i] !== '*' && word[i] === letterSet[i]) {
-        result = false;
-      }
-    }
-  });
-  return result;
-}
-
-function rankWords (words) {
-  let wordRanks = [];
-  let sortedWords = [];
-  words.forEach(word => {
-    let letterCount = {};
-    for (let i = 0; i < word.length; i++) {
-      if (letterCount[word[i]]) {
-        letterCount[word[i]]++;
-      } else {
-        letterCount[word[i]] = 1;
-      }
-    }
-    let rank = 1;
-    Object.values(letterCount).forEach(count => {
-      rank *= count;
-    });
-    wordRanks.push({ 'word': word, 'rank': rank });
-  });
-  wordRanks.sort((a, b) => a['rank'] - b['rank']);
-  wordRanks.forEach(rankedWord => {
-    sortedWords.push(rankedWord['word']);
-  });
-  return sortedWords;
-}
